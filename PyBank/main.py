0e92ac564e7697aabb288b98b84e9346e4a136c2
xzhoward <107507099+xzhoward@users.noreply.github.com>
2022-07-14 17:58:56 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,4 @@
+
 # Import os module that has functions to interact with the file system
 import os
 # Import module for reading CSV files
@@ -6,53 +7,42 @@ import csv
 #Get current working directory
 cwkdir = os.getcwd()
 #Append file directory and make a complete file path
-filepath = os.path.join( cwkdir,'Resources','election_data.csv')
+filepath = os.path.join( cwkdir,'PyBank','Resources','budget_data.csv')
 
 #Initialize variables
-totalcount = 0; kcount = 0; ccount = 0; lcount = 0; ocount = 0; max_votecount = 0
-
-#Function for % calculation
-def percentage (part, whole):
-    return 100 * float(part)/float(whole)
+mcount = 0; total = 0; PreValue = 0; Diff = 0; DiffMax = 0; DiffMin = 0
 
 #Open and read CSV file
 with open(filepath, newline='') as csvfile:
      csvreader = csv.reader(csvfile, delimiter=',')
-
+     csv_header = next(csvreader)
+     print(f'Financial Analysis'+'\n')
+     print(f'----------------------------'+'\n')
      for i in csvreader:
-         voterid = i[0]
-         country = i[1]
-         candidate = i[2]
-         # Find Total Vote Count
-         totalcount = totalcount + 1
+         month = i[0]
+         Amount = i[1]
+         iAmount = int(Amount)
+         Diff =  iAmount - PreValue
+         #Placeholder to track greatest increase in profits (financial analysis)
+         if DiffMax < Diff:
+            DiffMax = Diff
+            DiffMaxDate = month
+         #Placeholder to track greatest decrease in profits (financial analysis)
+         if DiffMin > Diff:
+            DiffMin = Diff
+            DiffMinDate = month
 
-         # Find votecount by candidates
-         if candidate =="Khan":
-            kcount = kcount + 1
-         if candidate =="Correy":
-            ccount = ccount + 1
-         if candidate =="Li":
-            lcount = lcount + 1
-         if candidate =="O'Tooley":
-            ocount = ocount + 1
-            
-# Define (dictionary) list : candidate and votes
-     candidatevote = {"Khan": kcount,"Correy": ccount,"Li" :lcount, "O'Tooley": ocount}
-     # Find winner 
-     for candidate, value in candidatevote.items():
-         if value > max_votecount:
-            max_votecount = value
-            winner = candidate
-# Display results       
-print(f'Election Results'+'\n')
-print(f'-------------------------------'+'\n')
-print(f'Total Votes: {totalcount}'+'\n')
-print(f'-------------------------------'+'\n')
+         PreValue = iAmount   
+         # Get total months (financial analysis)
+         mcount = mcount + 1
+         total += int(Amount) 
 
-print(f'Khan: {percentage(kcount,totalcount):.3f}%  ({kcount})')
-print(f'Correy: {percentage(ccount,totalcount):.3f}%  ({ccount})')
-print(f'Li: {percentage(lcount,totalcount):.3f}%  ({lcount})')
-print(f'O\'Tooley: {percentage(ocount,totalcount):.3f}%  ({ocount})')
-print(f'--------------------------------'+'\n')
-print(f'Winner: {winner} '+'\n')
-print(f'--------------------------------'+'\n')
+## Display Results ##      
+#The total number of months included in the dataset
+print(f'Total Months : {mcount}')
+#The total net amount of "Profit/Losses" over the entire period
+print(f'Total: $ {total}')
+# Greatest increase in profit
+print(f'Greatest Increase in Profits: {DiffMaxDate} : ($ {DiffMax})')
+# Greatest increase in profit
+print(f'Greatest Decrease in Profits: {DiffMinDate} : ($ {DiffMin})')
